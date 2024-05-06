535db6c014410cfe32b14113c50652037111f838
Mario Danner <s2310824003@fhooe.at>
2024-05-06 18:51:31 +0200

    Reset android.java

--- Koller1_Danner2_android/android.java
+++ Koller1_Danner2_android/android.java
@@ -2,11 +2,5 @@
 public class android {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-	System.out.println("New Line");
     }
-<<<<<<< HEAD
-    public static void thisIsANewMethod() {}
 }
-=======
-}
->>>>>>> 6a6dc44b7068ac726361995feb8236af4a421458
