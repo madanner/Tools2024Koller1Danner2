4a38639c5e9804f7c55aa1bc83d2a9c0e41b6b6c
Mario Danner <s2310824003@fhooe.at>
2024-05-06 16:34:31 +0200

    Fix

--- Koller1_Danner2_android/android.java
+++ Koller1_Danner2_android/android.java
@@ -4,4 +4,9 @@ public class android {
         System.out.println("Hello, World!");
 	System.out.println("New Line");
     }
+<<<<<<< HEAD
 }
+=======
+    public static void thisIsANewMethod() {}
+}
+>>>>>>> b02ee4a (Added new method in android.java)
